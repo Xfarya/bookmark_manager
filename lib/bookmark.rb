d611bcc43a3a3c6ac8283c2a45e7e7b5d2da0780
Farya <farya.hsn@gmail.com>
2021-08-17 16:00:25 +0200

    interacting with postgres +notes

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,9 +1,21 @@
+require 'pg'
+
 class Bookmark
-  def self.all
-    [
-      "http://www.makersacademy.com",
-      "http://www.destroyallsoftware.com",
-      "http://www.google.com"
-    ].join(', ')
+
+  def self.all #look at last challenge from Battle, to check 'self'
+    # [
+    #   "http://www.makersacademy.com",
+    #   "http://www.destroyallsoftware.com",
+    #   "http://www.google.com"
+    # ].join(', ')
+    con = PG.connect :dbname => 'bookmark_manager' #creates a connection to database
+    runsql = con.exec "SELECT * FROM bookmarks" #runs SQL - telling it which table to look at
+#first way
+    # runsql.each do |row|
+    #   puts "%s %s" % [ row['id'], row['url']]
+    # end
+      #%s is a placeholder for filling columns with relative values
+#second way
+    runsql.map { |row| row['url'] }.join(', ')
   end
 end
--- spec/unit/bookmark_spec.rb
+++ spec/unit/bookmark_spec.rb
@@ -3,9 +3,9 @@ require 'bookmark'
 describe Bookmark do
   it 'shows a list of all its bookmarks' do
     bookmarks = [
-      "http://www.makersacademy.com",
-      "http://www.destroyallsoftware.com",
-      "http://www.google.com"
+      "http://www.makersacademy.com/",
+      "http://www.google.com/",
+      "http://www.destroyallsoftware.com"
     ].join(', ')
 
     expect(described_class.all).to eq(bookmarks)
